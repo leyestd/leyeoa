341967de3cfb0fa645c4e5001fe7fa00ff910f2f
leyestd <303650172@qq.com>
2014-10-17 17:00:13 +0800

    修改流程

--- src/frontend/controller/Rdepartmentflow.java
+++ src/frontend/controller/Rdepartmentflow.java
@@ -26,70 +26,32 @@ public class Rdepartmentflow extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		int accountid = (Integer) request.getSession().getAttribute("id");
-		HashMap<Integer, RbacAccount> rbac = (HashMap<Integer, RbacAccount>) getServletContext().getAttribute("rbac");
-		HashMap<Integer, RbacRole> roles = (HashMap<Integer, RbacRole>) getServletContext().getAttribute("roles");
-
-		boolean approval = false;
-		int approvalRoleId = 0;
+		HashMap<Integer, RbacAccount> rbac = (HashMap<Integer, RbacAccount>) getServletContext()
+				.getAttribute("rbac");
+		HashMap<Integer, RbacRole> roles = (HashMap<Integer, RbacRole>) getServletContext()
+				.getAttribute("roles");
 
 		// 可审批用户都可查询本部已审批单据
-		// 如果我有approval角色
 
-		if (rbac.get(accountid).getRole().contains("Approval")) {
-			approval = true;
+		int pageNumber;
+		if (request.getParameter("pageNumber") == null) {
+			pageNumber = 1;
 		} else {
-			Set<Integer> roleskey = roles.keySet();
-			for (Integer key : roleskey) {
-				if (roles.get(key).getName().equals("Approval")) {
-					approvalRoleId = key;
-					break;
-				}
-			}
-			if (approvalRoleId != 0) {
-				ArrayList<Integer> basicRole = D_Role_Hierarchy
-						.doSelectAdvanced(approvalRoleId);
-				for (int id : basicRole) {
-					if (roles.get(id).getUser().containsKey(accountid)) {
-						approval = true;
-						break;
-					}
-				}
-			}
+			pageNumber = Integer.valueOf(request.getParameter("pageNumber"));
 		}
 
-		if (approval == false) {
-			if (rbac.get(accountid).getRole().contains("DepartmentForm")) {
-				approval = true;
-			}
-		}
+		String condition = " WHERE roleflow REGEXP '^"
+				+ 1
+				+ "(,[[:digit:]]+)*$' AND status != 0 AND custom = 'f' ORDER BY id DESC";
+		Pagination page = new Pagination(pageNumber, 10, "workflow", condition);
 
-		if (approval == true) {
-			int default_roleid = rbac.get(accountid).getDefault_roleid();
-
-			int pageNumber;
-			if (request.getParameter("pageNumber") == null) {
-				pageNumber = 1;
-			} else {
-				pageNumber = Integer
-						.valueOf(request.getParameter("pageNumber"));
-			}
-
-			String condition = " WHERE roleflow REGEXP '^"
-					+ default_roleid
-					+ "(,[[:digit:]]+)*$' AND status != 0 AND custom = 'f' ORDER BY id DESC";
-			Pagination page = new Pagination(pageNumber, 10, "workflow",
-					condition);
-
-			if (page.getTotal() != 0) {
-				String[] columns = { "id", "name", "account_id", "createtime",
-						"status" };
-				List<ArrayList<Object>> rows = page.getRows(columns);
-				request.setAttribute("rows", rows);
-				request.setAttribute("pageNumber", pageNumber);
-				request.setAttribute("countPage", page.getCountPage());
-			}
-		} else {
-			request.setAttribute("approval", "denied");
+		if (page.getTotal() != 0) {
+			String[] columns = { "id", "name", "account_id", "createtime",
+					"status" };
+			List<ArrayList<Object>> rows = page.getRows(columns);
+			request.setAttribute("rows", rows);
+			request.setAttribute("pageNumber", pageNumber);
+			request.setAttribute("countPage", page.getCountPage());
 		}
 
 		String url = "/WEB-INF/frontend/rdepartmentflow.jsp";
